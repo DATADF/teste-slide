b04757161b9c3389f51c5758a12220a699070e71
DATADF <klaytonfantin@outlook.com>
2019-07-17 22:59:43 -0300

    Dropdown

--- js/banner.js
+++ js/banner.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
             menu = target;
         $target = $(target);
         $('html, body').stop().animate({
-            'scrollTop': $target.offset().top + 2
+            'scrollTop': $target.offset().top + 3
         }, 500, 'swing', function () {
             window.location.hash = target;
             $(document).on("scroll", onScroll);
